ab78c7183ddc8ffe27eefbfba36a2d298bd9fc88
Emma Smith Zbarsky <esmithzb@mathworks.com>
2024-10-15 14:52:29 -0400

    Simplify structure of FunctionTest

--- SoftwareTests/FunctionTests.m
+++ SoftwareTests/FunctionTests.m
@@ -1,7 +1,5 @@
 classdef FunctionTests < matlab.unittest.TestCase
 
-    % https://www.mathworks.com/help/matlab/matlab_prog/use-parameters-in-class-based-tests.html
-
     methods(Test)    
 
     end % methods
